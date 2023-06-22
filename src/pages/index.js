edb3157250491a6f7ed95622edb78966e87e00fd
CtzarGM <cesarga4649@google.com>
2023-06-22 15:45:05 +0200

    placeholder is visible

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <div className={styles.container}>
+          <div id='navAvatar' className={styles.navAvatar} style={{ top: '200px', left: '500px' }}>nav</div>
+        </div>
 
         {/* <div className={styles.description}>
           <p>
