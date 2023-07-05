78fa9f5f8f57c4b4fd5a0ecabb0614086a9c93d1
CtzarGM <cesarga4649@google.com>
2023-07-05 04:34:21 +0200

    only check for collisions if avatar is moving

--- src/pages/index.js
+++ src/pages/index.js
@@ -179,8 +179,11 @@ export default function Home() {
           closeMenu();
         }
 
-        detectCollision();
-        detectPlayerCollision();
+        if (isAvatarMoving) {
+          detectCollision();
+          detectPlayerCollision();
+        }
+
 
         window.requestAnimationFrame(moveLoop);
       }
