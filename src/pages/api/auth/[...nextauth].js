ef941c209dc1ff6fb9f1049eb222fecf8711624f
CtzarGM <cesarga4649@google.com>
2023-07-06 11:49:58 +0200

    test redirect to myavatar

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -22,6 +22,24 @@ export const authOptions = {
             },
         }),
     ],
+
     adapter: MongoDBAdapter(clientPromise),
+    callbacks: {
+        session: async ({ session, token }) => {
+            if (session?.user) {
+                session.user.id = token.uid;
+            }
+            return session;
+        },
+        jwt: async ({ user, token }) => {
+            if (user) {
+                token.uid = user.id;
+            }
+            return token;
+        },
+    },
+    session: {
+        strategy: 'jwt',
+    },
 }
 export default NextAuth(authOptions)
--- src/pages/myavatar/index.js
+++ src/pages/myavatar/index.js
@@ -1,7 +1,26 @@
-export default function MyAvatar() {
+import { useSession, signIn, signOut } from "next-auth/react"
+export default function Component() {
+
+    const { data: session } = useSession()
+
+    console.log(session)
+
+    if (session) {
+        return (
+            <>
+                <h3>
+                    <img src={session.user.image} style={{ width: '100px', borderRadius: '50%' }} />
+                    Signed in as {session.user.name}
+                </h3>
+                <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
     return (
         <>
-            <h1>hello</h1>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
         </>
-    );
+    )
 }
